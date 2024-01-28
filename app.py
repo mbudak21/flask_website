c27de25669882df18eb2d89eccc7db6ca688af80
Mehmet Murat Budak <mbudak21@ku.edu.tr>
2024-01-28 21:35:18 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+@app.route('/')
+def main_page():
+    return render_template('main.html')
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
